87a191da74c8ec2bcbe45909f6d5e77664e5ab14
Jiahao Chen <jiahao@mit.edu>
2015-05-05 00:38:12 -0400

    Rewrite gkvol to be much faster

--- src/volatility.jl
+++ src/volatility.jl
@@ -1,80 +1,59 @@
 @doc """Garman-Klass volatility
 
 Input:
-
-    times: the time stamps parsed into DateTimes
-    data: the raw nxcore trade data
     tpv: time-price-volume DataFrame
     blocklength: length of time interval to find volatility over. Default: Dates.Minute(15)
     interval: time between successive time intervals. Default: Dates.Minute(1))
-    res: Time resolution to compute volatilies over. Default: Dates.Millisecond(25)
+    res: Time resolution to compute volatilities over. Default: Dates.Millisecond(25)
 """ ->
-function gkvol(data,  times = parse_times(data),
-    tpv::DataFrames.DataFrame=parse_tsv(data, true),
+function gkvol(tpv::DataFrame;
     blocklength=Dates.Minute(15), interval=Dates.Minute(1),
     res=Dates.Millisecond(25))
 
+    const times=tpv[:, :time]
     tmin, tmax = extrema(times)
     starttimeblock = tmin
     endtimeblock = tmin+blocklength
-    blocktimes = Any[]
+    blocktimes = Dates.DateTime[]
     volatilities = Float64[]
 
     const k=2log(2)-1
+    i1=1
     while starttimeblock < tmax
-        VolGK = 0.0
-        pricehi = 0.0
-        pricelo = Inf
-        pricecl, timecl = 0.0, starttimeblock
-        priceop, timeop = 0.0, endtimeblock
-        for (i, t) in enumerate(times)
-            starttimeblock ≤ t < endtimeblock || continue
-
-            pricehi = max(pricehi, data[i, 31])
-            data[i, 32]==0 || (pricelo = min(pricelo, data[i, 32])) #Guard against bad data with price 0
-            if abs(t-starttimeblock) < abs(timecl-starttimeblock)
-                timeop = t
-                priceop = data[i, 30]
-            end
-            if abs(t-endtimeblock) < abs(timecl-endtimeblock)
-                timecl = t
-                pricecl = data[i, 33]
-            end
-        end
+        t1 = sub(times, i1:size(times,1))
+        i1 = findfirst(x->x>starttimeblock, t1)+i1-1
+        t2 = sub(times, i1:size(times,1))
+        i2 = findfirst(x->x≥endtimeblock, t2)+i1-1
+        i2==i1-1 && (i2=size(tpv, 1))
+        priceop = tpv[i1, :price]
+        pricecl = tpv[i2, :price]
+        pricelo, pricehi = extrema(tpv[i1:i2, :price])
 
         #Average price properly, taking into account that price is not
         #always reported in uniform time intervals
         timeblock = starttimeblock:res:endtimeblock
-        pricesblock = tpv[starttimeblock .≤ tpv[:, :time] .< endtimeblock, :]
         lastprice = priceop
         pricesum = 0.0
-        tidx = 1
-        for i=1:size(pricesblock, 1)
-            thistime = pricesblock[i, :time]
-            while timeblock[tidx] < thistime
-                tidx += 1
-                pricesum += lastprice
-            end
-            lastprice = pricesblock[i, :price]
+        lasttime = starttimeblock
+        for i=i1:i2
+            thistime = tpv[i, :time]
+            ntimes = int((thistime-lasttime)÷res)
+            lasttime += ntimes*res
+            pricesum += ntimes*lastprice
+            lastprice = tpv[i, :price]
         end
-        while timeblock[tidx] < endtimeblock
-            tidx += 1
-            pricesum += lastprice
-        end
-        priceavg = pricesum / length(timeblock)
+        ntimes = int((endtimeblock-lasttime)÷res)
+        lasttime += ntimes*res
+        pricesum += ntimes*lastprice
+        priceavg = pricesum/length(timeblock)
 
-        if !isfinite(pricelo)
-            VolGK = 0.0
-        else
-            VolGK = √(0.5*(pricehi-pricelo)^2 - k*(pricecl-priceop)^2)/priceavg
-        end
+        VolGK = √(0.5*(pricehi-pricelo)^2 - k*(pricecl-priceop)^2)/priceavg
 
         starttimeblock += interval
-        endtimeblock += interval
+        endtimeblock   += interval
 
         push!(blocktimes, starttimeblock)
         push!(volatilities, VolGK)
     end
     DataFrames.DataFrame(time = blocktimes, gk_volatility = volatilities)
 end
-
